31813d51e5cd0cc18e33d0e3c0c264bb7eb8b756
dylanmeuriot <dylanmeuriot@gmail.com>
2021-10-18 12:54:05 +0200

    Commit 3 : pages

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { OrdersRoutingModule } from './orders-routing.module';
-
+import { PageAddOrderComponent } from './pages/page-add-order/page-add-order.component';
+import { PageEditOrderComponent } from './pages/page-edit-order/page-edit-order.component';
+import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    OrdersRoutingModule
-  ]
+  declarations: [
+    PageListOrdersComponent,
+    PageAddOrderComponent,
+    PageEditOrderComponent,
+  ],
+  imports: [CommonModule, OrdersRoutingModule],
 })
-export class OrdersModule { }
+export class OrdersModule {}
