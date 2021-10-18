97e6eb3eee6f68aaf76e05dd6c6b44a1910f5656
dylanmeuriot <dylanmeuriot@gmail.com>
2021-10-18 16:17:36 +0200

    Commit 7 : la transclusion

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,21 @@ import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { UiModule } from '../ui/ui.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, NavComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [UiModule, IconsModule, TemplatesModule, LoginModule],
+  exports: [
+    UiModule,
+    IconsModule,
+    TemplatesModule,
+    LoginModule,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+  ],
 })
 export class CoreModule {}
