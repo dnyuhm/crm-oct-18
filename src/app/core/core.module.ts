9e71ea3cd1475102ceb901090732d726c85b5d23
dylanmeuriot <dylanmeuriot@gmail.com>
2021-10-19 14:37:00 +0200

    Commit 17 : Navigation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
-import { OrdersModule } from '../orders/orders.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { UiModule } from '../ui/ui.module';
 import { FooterComponent } from './components/footer/footer.component';
@@ -11,7 +11,7 @@ import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [HeaderComponent, NavComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [
     UiModule,
     IconsModule,
