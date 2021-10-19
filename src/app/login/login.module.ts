ac44a7227100f14800271897890ffd7174235c94
dylanmeuriot <dylanmeuriot@gmail.com>
2021-10-19 14:19:33 +0200

    Commit 16 : Routes orders clients  page not found

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,23 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { LoginRoutingModule } from './login-routing.module';
+import { PageForgotPasswordComponent } from './pages/page-forgot-password/page-forgot-password.component';
+import { PageResetPasswordComponent } from './pages/page-reset-password/page-reset-password.component';
 import { PageSignInComponent } from './pages/page-sign-in/page-sign-in.component';
 import { PageSignUpComponent } from './pages/page-sign-up/page-sign-up.component';
-import { PageResetPasswordComponent } from './pages/page-reset-password/page-reset-password.component';
-import { PageForgotPasswordComponent } from './pages/page-forgot-password/page-forgot-password.component';
-
 
 @NgModule({
   declarations: [
     PageSignInComponent,
     PageSignUpComponent,
     PageResetPasswordComponent,
-    PageForgotPasswordComponent
+    PageForgotPasswordComponent,
   ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+  imports: [CommonModule, LoginRoutingModule],
 })
-export class LoginModule { }
+export class LoginModule {}
